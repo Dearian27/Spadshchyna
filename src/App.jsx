a276a4bc39294fc05a9ea0ecc807f80d2db5abae
Dearian <dearian2706@gmail.com>
2023-07-23 23:55:08 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Background } from './components/Background';
 import { AdaptiveDpr, AdaptiveEvents, Html, SoftShadows } from '@react-three/drei';
 import { Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import { Kolomya } from './components/locations/Kolomya';
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/eggs' element={<Kolomya />} />
       </Routes>
     </>
   );
