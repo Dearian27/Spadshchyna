705bff552dea418880caf0e0711536734237e6f1
Dearian <dearian2706@gmail.com>
2023-07-26 12:04:02 +0300

    remove HEADs

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      {/* <Leva /> */}
-    </BrowserRouter>
-  // </React.StrictMode>,
-=======
 import { raf } from '@react-spring/rafz';
 
 raf.frameLoop = 'always';
@@ -25,5 +15,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Leva hidden />
     </BrowserRouter>
   </>,
->>>>>>> develop
 );
