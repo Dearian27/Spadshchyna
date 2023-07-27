898866ba7b592520c882a8372c4284fffaa032af
Serhii <sirozhka.v@gmail.com>
2023-07-27 17:26:27 +0200

    mark description feature, new backround and hdri

--- src/components/WaterPlane.jsx
+++ src/components/WaterPlane.jsx
@@ -19,9 +19,9 @@ export function WaterPlane(props) {
   return (
     <group position={[-0.5, 0.01, 0]} {...props} dispose={null}>
       <animated.group scale={scale}>
-        <mesh geometry={nodes.Circle_1.geometry} material={materials.layer1} />
-        <mesh geometry={nodes.Circle_2.geometry} material={materials.layer2} />
-        <mesh geometry={nodes.Circle_3.geometry} material={materials.layer3} />
+        <mesh receiveShadow geometry={nodes.Circle_1.geometry} material={materials.layer1} />
+        <mesh receiveShadow geometry={nodes.Circle_2.geometry} material={materials.layer2} />
+        <mesh receiveShadow geometry={nodes.Circle_3.geometry} material={materials.layer3} />
       </animated.group>
     </group>
   );
--- src/pages/locations/KolomuyaScene.jsx
+++ src/pages/locations/KolomuyaScene.jsx
@@ -1,9 +1,8 @@
-import { OrbitControls, PointerLockControls } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
 import Lights from '../../components/Lights';
 import { Background } from '../../components/Background';
 import { Eggscene } from '../../components/Eggscene';
-import { useSpring, animated } from '@react-spring/three';
 import Framer from '../Framer';
 
 const KolomuyaScene = () => {
@@ -11,21 +10,17 @@ const KolomuyaScene = () => {
     <>
       <Framer>
         <div style={{ position: 'fixed', height: '100vh', width: '100%' }}>
-          <Canvas shadows camera={{ position: [0, 10, 15], fov: 30 }}>
+          <Canvas shadows camera={{ position: [0, 7, 15], fov: 30 }}>
             <Background />
             <ambientLight intensity={0.4} />
-            <Lights />
-
             <OrbitControls
-              enableZoom={false}
-              makeDefault
-              minPolarAngle={Math.PI / 3}
-              maxPolarAngle={Math.PI / 3}
-              // minDistance={7}
-              // maxDistance={20}
+            //   enableZoom={false}
+            //   makeDefault
+            //   minPolarAngle={Math.PI / 3}
+            //   maxPolarAngle={Math.PI / 3}
             />
-            {/* <PointerLockControls /> */}
-            <Eggscene position={[1, 0, -2]} rotation-y={(2 * Math.PI) / 6} />
+            <Lights />
+            <Eggscene position={[5, 0, -2]} rotation-y={(2 * Math.PI) / 6} />
           </Canvas>
         </div>
       </Framer>
